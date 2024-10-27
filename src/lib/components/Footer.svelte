cc487423bf6e3432a4d7c8fa560f91e66592233d
dtyronestrand <134752+dtyronestrand@noreply.githubuser.com>
2024-10-26 20:28:43 -0400

    first commit

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Wordmark from "./Wordmark.svelte";
+    /**@type{import("@prismicio/client").Content.SettingsDocument}*/
+    export let settings;
+</script>
+
+<nav>
+    <a href="/"><Wordmark/>
+    <span class="sr-only">{settings.data.site_title}</span>
+    </a>
+</nav>
---
+++ src/lib/components/Wordmark.svelte
@@ -0,0 +1,6 @@
+
+<svg id="Layer_1" xmlns="http://www.w3.org/2000/svg" baseProfile="tiny" version="1.2" viewBox="0 0 84.65 99.92">
+  <!-- Generator: Adobe Illustrator 29.0.0, SVG Export Plug-In . SVG Version: 2.1.0 Build 186)  -->
+  <text transform="translate(0 54.14)" font-family="ProtestRevolution-Regular, &apos;ProtestRevolution Regular&apos;" font-size="76.25"><tspan x="0" y="0">NL</tspan></text>
+  <text transform="translate(.65 96.89)" font-family="ProtestRevolution-Regular, &apos;ProtestRevolution Regular&apos;" font-size="43.25"><tspan x="0" y="0">MAF</tspan></text>
+</svg>
