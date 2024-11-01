231912e02f8112c1a33b3bff1b777ff2cb599b29
dtyronestrand <153030742+dtyronestrand@users.noreply.github.com>
2024-11-01 17:41:30 +0000

    Bento and Program style changes

--- src/lib/slices/Bento/index.svelte
+++ src/lib/slices/Bento/index.svelte
@@ -19,22 +19,28 @@
 		<PrismicRichText field={slice.primary.body} />
 	</div>
 
-	<div class="mt-16 grid max-w-4xl grid-rows-[auto_auto_auto] gap-8 md:grid-cols-3 md:gap-10">
+	
 		{#each slice.primary.box as item}
 			<div
 				class={clsx(
-					'glass-container row-span-3 grid grid-rows-subgrid gap-4 rounded-lg bg-gray-950/60 p-4 before:bg-gray-100/10',
+					"card glass-card row-span-3 grid grid-rows-subgrid gap-4 rounded-lg bg-gray-950/60 p-4 before:bg-gray-100/10",
 					item.wide ? 'md:col-span-2' : 'md:col-span-1'
 				)}
 			>
+			<div class="flex text-balance text-center flex-row">
 				<h3 class="text-2xl">
 					<PrismicText field={item.title} />
 				</h3>
-				<div class="max-w-md text-balance text-gray-300">
+			</div>
+				<div class="prose pose-invert max-w-md text-balance text-gray-300">
 					<PrismicRichText field={item.body} />
 				</div>
-				<PrismicImage field={item.image}  />
+				<div class="avatar">
+					<div class="ring-primary ring-offset-base-100 w-24 rounded-full ring ring-offset-2">
+					  <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+					</div>
+				  </div>
 			</div>
 		{/each}
-	</div>
+
 </Bounded>
