7bd1fb8747b3d0e2176dbda5d890c24f142dd9dc
dtyronestrand <1354732+dtyronestrand@noreply.githubuser.com>
2024-11-17 00:12:58 -0500

    hook error

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,14 +1,8 @@
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
-import { createBrowserClient } from '@supabase/ssr'
+import type { HandleClientError } from '@sveltejs/kit'
 
-export const handleError = ({ error, event }) => {
-  console.error(error, event)
-  return {
-    message: 'An unexpected error occurred.',
-  }
+export const handleError: HandleClientError = ({ error, event }) => {
+    console.error('Client error:', error, event)
+    return {
+        message: 'An unexpected error occurred.',
+    }
 }
-
-createBrowserClient(
-  PUBLIC_SUPABASE_URL,
-  PUBLIC_SUPABASE_ANON_KEY
-)
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,40 @@
-import type { HandleClientError } from '@sveltejs/kit'
+import { createServerClient } from '@supabase/ssr'
+import type { Handle } from '@sveltejs/kit'
+import { type CookieSerializeOptions } from 'cookie'
 
-export const handleError: HandleClientError = ({ error, event }) => {
-    console.error('Client error:', error, event)
-    return {
-        message: 'An unexpected error occurred.',
+export const handle: Handle = async ({ event, resolve }) => {
+    event.locals.supabase = createServerClient(
+        import.meta.env.VITE_PUBLIC_SUPABASE_URL,
+        import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY,
+        {
+            cookies: {
+                get: (key) => event.cookies.get(key),
+                set: (key, value, options: CookieSerializeOptions) => {
+                    event.cookies.set(key, value, {
+                        ...options,
+                        path: '/'
+                    })
+                },
+                remove: (key, options: CookieSerializeOptions) => {
+                    event.cookies.delete(key, {
+                        ...options,
+                        path: '/'
+                    })
+                },
+            },
+        }
+    )
+
+    event.locals.getSession = async () => {
+        const {
+            data: { session },
+        } = await event.locals.supabase.auth.getSession()
+        return session
     }
+
+    return resolve(event, {
+        filterSerializedResponseHeaders(name) {
+            return name === 'content-range'
+        },
+    })
 }
