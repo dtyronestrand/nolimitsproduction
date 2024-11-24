d5d5573ef25aba2bbed0b049932bd96d5da66303
dtyronestrand <1354732+dtyronestrand@noreply.githubuser.com>
2024-11-24 01:23:48 -0500

    installed skeleton ui

---
+++ tailwind.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from 'tailwindcss'
+import { join } from 'path'
+import { skeleton } from '@skeletonlabs/tw-plugin'
+import { noLimitsTheme } from './nolimits'
+
+const config: Config = {
+  darkMode: 'class',
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+    // This includes all Skeleton UI components
+    join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    skeleton({
+      themes: {
+        custom: [
+          noLimitsTheme
+        ]
+      }
+    })
+  ]
+}
+
+export default config
